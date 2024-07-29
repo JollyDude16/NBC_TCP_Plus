6f4a686daebf63238de176028bda19752e1db9e9
민광규 <167201080+JollyDude16@users.noreply.github.com>
2024-07-29 15:42:04 +0900

    ✨ update: 3주차 강의 유저, 게임 클래스

--- src/constants/env.js
+++ src/constants/env.js
@@ -6,7 +6,7 @@ export const PORT = process.env.PORT || 5555;
 export const HOST = process.env.HOST || 'localhost';
 export const CLIENT_VERSION = process.env.CLIENT_VERSION || '1.0.0';
 
-export const DB1_NAME = process.env.DB1_NAME || 'database1';
+export const DB1_NAME = process.env.DB1_NAME;
 export const DB1_USER = process.env.DB1_USER || 'user1';
 export const DB1_PASSWORD = process.env.DB1_PASSWORD || 'password1';
 export const DB1_HOST = process.env.DB1_HOST || 'localhost';
--- src/init/index.js
+++ src/init/index.js
@@ -1,14 +1,14 @@
 // 서버 초기화 작업
-import pools from '../db/database.js';
-import { testAllConnections } from '../utils/db/testConnection.js';
 import { loadGameAssets } from './assets.js';
 import { loadProtos } from './loadProtos.js';
+import { testAllConnections } from '../utils/db/testConnection.js';
+import pools from '../db/database.js';
 
 const initServer = async () => {
   try {
     await loadGameAssets();
     await loadProtos();
-    await testAllConnections(pools)
+    await testAllConnections(pools);
     // 다음 작업
   } catch (e) {
     console.error(e);
